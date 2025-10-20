45e3a48f624b4e76a2dfe72496356a6aa0c9a967
rulasssdeep <146976056+rulosodeep@users.noreply.github.com>
2025-10-20 15:27:09 -0600

    er

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -3,6 +3,9 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
+  getHello(): any {
+    throw new Error('Method not implemented.');
+  }
   @UseGuards(JwtAuthGuard)
   @Get('auth/me')
   me(@Req() req: any) {
